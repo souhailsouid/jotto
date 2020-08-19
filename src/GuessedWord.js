4dc8811c3433e48107410db57e21254014eb35b4
Souhail SOUID <mr.souid@live.fr>
2020-08-19 10:51:52 +0200

    tests and code for non-zero guessed words

--- src/GuessedWord.js
+++ src/GuessedWord.js
@@ -16,7 +16,28 @@ const GuessedWord = (props) => {
               Try to guess the word ! 
           </span>
         )
-    } 
+    } else {
+        const guessedWordRows = props.guessedWords.map((word, index) => (
+            <tr data-test="guessed-word" key={index}>
+                <td> {word.guessedWord} </td>
+                <td> {word.letterMatchCount} </td>
+            </tr>
+        ))
+        contents = (
+            <div data-test="guessed-words">
+               <h3> Guessed Words </h3>
+                <table>
+                    <thead>
+                        <tr><th>Guess</th></tr>
+                        <tr><th>Matching letters</th></tr>
+                    </thead>
+                    <tbody>
+                        { guessedWordRows }
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
     return (
       <div data-test="component-guessed-words">
         { contents }
