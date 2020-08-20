67f93399162c49de152941c348aebb981cebb57d
Souhail SOUID <mr.souid@live.fr>
2020-08-20 09:36:06 +0200

    configure redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
+import { Provider }  from 'react-redux'
 import App from './App';
+import store from './configureStore'
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
