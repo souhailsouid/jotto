686c5fea43c3390ebcb516a602e89398676f9fc2
Souhail SOUID <mr.souid@live.fr>
2020-08-21 10:12:05 +0200

    setup guessedwords test

--- src/actions/index.test.js
+++
@@ -1,8 +0,0 @@
-import { actionTypes, correctGuess} from './index'
-
-describe('correct guess' , () => {
-    const action = correctGuess()
-    test(' should action the corect actionTypes' , () => {
-        expect(action).toEqual({type: actionTypes.CORRECT_GUESS})
-    })
-})
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers} from 'redux'
 import success from './successReducer'
-
+import guessedWords from './guessedWordsReducer'
 export default combineReducers({
-    success
+    success,
+    guessedWords
 })
