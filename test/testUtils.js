38af9cc673c371ede222a2861639e26bedd5fb3b
Souhail SOUID <mr.souid@live.fr>
2020-08-21 09:34:15 +0200

    write store for testing

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,5 +1,9 @@
 import checkPropTypes from 'check-prop-types'
-
+import { createStore } from 'redux'
+import rootReducer from '../src/reducers'
+export const storeFactory = (initialState) => {
+    return createStore(rootReducer, initialState)
+}
 export const findByTestAttr = (wrapper, value) => {
    return  wrapper.find(`[data-test="${value}"]`)
 }
