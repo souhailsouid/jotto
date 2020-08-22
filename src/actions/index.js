b611615219f5f995265a28bcc823883bdf388211
Souhail SOUID <mr.souid@live.fr>
2020-08-22 09:56:52 +0200

    add secretWord action creator and reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
 import {getLetterMatchCount} from '../helpers/index'
+import axios from 'axios'
 
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
-    GUESS_WORD: 'GUESS_WORD'
+    GUESS_WORD: 'GUESS_WORD',
+    SET_SECRET_WORD: 'SET_SECRET_WORD'
 }
 
 export const guessWord = (guessedWord) => {
@@ -13,9 +15,20 @@ export const guessWord = (guessedWord) => {
             type: actionTypes.GUESS_WORD,
             payload: { guessedWord, letterMatchCount}
         })
-        
+
         if(guessedWord === secretWord) {
             dispatch({type: actionTypes.CORRECT_GUESS })
         }
+    }
+}
+export const getSecretWord = () => {
+    return (dispatch) => {
+        return axios.get("http:localhost:3030")
+            .then(response => {
+                dispatch({
+                    type: actionTypes.SET_SECRET_WORD,
+                    payload: response.data
+                })
+            })
     }
 }
